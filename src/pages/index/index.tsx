cac75b416cf9c9fc9e61ed46e9da1ed66780546b
姜衿 <70458277@qq.com>
2023-02-09 11:35:16 +0800

    第一次提交

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,36 @@
+import { Component, PropsWithChildren } from "react";
+import {
+  Button,
+  Cell,
+} from "@nutui/nutui-react-taro";
+import './index.scss'
+
+class Index extends Component<PropsWithChildren> {
+   constructor(props: any) {
+     super(props);
+   }
+
+   componentDidMount() {}
+
+   componentWillUnmount() {}
+
+   componentDidShow() {}
+
+   componentDidHide() {}
+
+   render() {
+     return (
+       <div className="nutui-react-demo">
+         <div className="index">
+          欢迎使用 NutUI React 开发 Taro 多端项目。
+         </div>
+         <div className="index">
+           <Button type="primary" className="btn">
+             NutUI React Button
+           </Button>
+         </div>
+       </div>
+     );
+   }
+}
+export default Index
