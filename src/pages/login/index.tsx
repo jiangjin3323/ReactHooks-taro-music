e87dab0c916a2719dd21bee87a1e4efa80223f68
姜衿 <70458277@qq.com>
2023-02-27 17:53:18 +0800

    仿网易云音乐

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "@tarojs/components";
+import { Button } from '@nutui/nutui-react-taro';
+
+const App:React.FC = () =>{
+    return (
+        <View>
+            <Button type="default">登陆</Button>
+        </View>
+    )
+}
+export default App;
