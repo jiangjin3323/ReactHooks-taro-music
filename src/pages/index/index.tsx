37e844143994182222d674e342ca2f8d216b1e77
姜衿 <70458277@qq.com>
2023-02-28 16:37:08 +0800

    首页ui调整完成！

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -46,46 +46,25 @@ const App: React.FC = () => {
               </View>
             </View>
           </View>
-          <View className="wrap-content-body-item">
-            <View className="wcbi-body">
-              <Image className="wcbi-body-img" src="http://p1.music.126.net/hLv4YJOiLTE16LlRpdxZgQ==/109951164152314267.jpg?imageView=1&type=webp&thumbnail=246x0" />
-              <View className="wcbi-body-text">
-                <Icon name="service" size={12}></Icon>
-                <Text className="wcbi-body-text-num">
-                  4032万
-                </Text>
-              </View>
-              <View className="wcbi-body-bottom">
-                用音乐保持你每天的嘴角上扬
-              </View>
+        </View>
+        <View className="wrap-content-top">
+          <View className="wrap-content-top-title">
+            <View className="wctt-placeholder"></View>
+            <View className="wctt-text">
+              最新音乐
             </View>
           </View>
-          <View className="wrap-content-body-item">
-            <View className="wcbi-body">
-              <Image className="wcbi-body-img" src="http://p1.music.126.net/hLv4YJOiLTE16LlRpdxZgQ==/109951164152314267.jpg?imageView=1&type=webp&thumbnail=246x0" />
-              <View className="wcbi-body-text">
-                <Icon name="service" size={12}></Icon>
-                <Text className="wcbi-body-text-num">
-                  4032万
-                </Text>
-              </View>
-              <View className="wcbi-body-bottom">
-                用音乐保持你每天的嘴角上扬
+        </View>
+        <View className="wrap-content-music">
+          <View className="wrap-content-music-item">
+            <View className="wcmi-left">
+              <View className="wcmi-left-name">今生今世</View>
+              <View className="wcmi-left-singer">
+                炎明熹-今生今世
               </View>
             </View>
-          </View>
-          <View className="wrap-content-body-item">
-            <View className="wcbi-body">
-              <Image className="wcbi-body-img" src="http://p1.music.126.net/hLv4YJOiLTE16LlRpdxZgQ==/109951164152314267.jpg?imageView=1&type=webp&thumbnail=246x0" />
-              <View className="wcbi-body-text">
-                <Icon name="service" size={12}></Icon>
-                <Text className="wcbi-body-text-num">
-                  4032万
-                </Text>
-              </View>
-              <View className="wcbi-body-bottom">
-                用音乐保持你每天的嘴角上扬
-              </View>
+            <View className="wcmi-right">
+              <Icon name="play-start" size={28} color={'RGBA(102, 102, 102, 1)'}></Icon>
             </View>
           </View>
         </View>
