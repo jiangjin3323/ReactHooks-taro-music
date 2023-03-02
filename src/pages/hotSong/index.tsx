bdf69bd0dcd1500dfcdde9062290817de89469f3
姜衿 <70458277@qq.com>
2023-03-02 15:17:24 +0800

    新增tabs 跳转

---
+++ src/pages/hotSong/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "@tarojs/components";
+const HotSong:React.FC = () =>{
+    return (
+        <View>
+            热歌榜
+        </View>
+    )
+}
+export default HotSong;
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "@tarojs/components";
+const Search:React.FC = () =>{
+    return (
+        <View>
+            搜索列表
+        </View>
+    )
+}
+export default Search;
