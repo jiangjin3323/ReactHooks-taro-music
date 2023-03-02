63f5c526e8aeab6b23281d2990967f463a64b969
姜衿 <70458277@qq.com>
2023-03-02 17:29:59 +0800

    搜索列表新增搜索框。

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { View } from "@tarojs/components";
 import TopHeader from "../../components/header/header";
+import { SearchBar } from '@nutui/nutui-react-taro';
 const Search:React.FC = () =>{
     return (
         <View className="wrap">
             {/* 自定义头部 */}
             <TopHeader selcetIndex={2}></TopHeader>
+            <SearchBar placeholder="搜索歌手，歌曲，专辑" />
         </View>
     )
 }
