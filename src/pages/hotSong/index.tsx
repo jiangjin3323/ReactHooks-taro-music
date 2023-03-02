2f165bb8fc9c4fc9684fe07ce83ca93d4eeb038e
姜衿 <70458277@qq.com>
2023-03-02 15:36:56 +0800

    新增热歌榜，搜索页。

--- src/pages/hotSong/index.tsx
+++ src/pages/hotSong/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { View } from "@tarojs/components";
+import TopHeader from "../../components/header/header";
+import Tabs from "../../components/tabs/tabs";
 const HotSong:React.FC = () =>{
     return (
-        <View>
-            热歌榜
+        <View className="wrap">
+            {/* 自定义头部 */}
+            <TopHeader></TopHeader>
+            <Tabs selcetIndex={1}></Tabs>
         </View>
     )
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { View } from "@tarojs/components";
+import TopHeader from "../../components/header/header";
+import Tabs from "../../components/tabs/tabs";
 const Search:React.FC = () =>{
     return (
-        <View>
-            搜索列表
+        <View className="wrap">
+            {/* 自定义头部 */}
+            <TopHeader></TopHeader>
+            <Tabs selcetIndex={2}></Tabs>
         </View>
     )
 }
