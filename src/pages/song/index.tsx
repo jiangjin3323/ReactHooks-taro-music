14313e60a1f9de29dc66fe69006a330eb124bd0a
姜衿 <70458277@qq.com>
2023-03-07 16:06:55 +0800

    代码优化。

--- src/pages/song/index.tsx
+++ src/pages/song/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import Taro from "@tarojs/taro";
-import { useLoad } from "@tarojs/taro";
+import { useLoad, useUnload } from "@tarojs/taro";
 import { View, Image } from "@tarojs/components";
 import { getMusicUrlApi, getMusicDetailApi } from "../../api/api";
-import { NavBar, Icon } from '@nutui/nutui-react-taro';
+import { NavBar, Icon,Toast } from '@nutui/nutui-react-taro';
 import './index.scss';
 import leverImg from '../../static/index/song_lever.png';
 import randomImg from '../../static/index/icon_song_random.png';
@@ -12,14 +12,18 @@ import pauseImg from '../../static/index/icon_song_pause.png';
 import leftImg from '../../static/index/icon_song_left.png';
 import rightImg from '../../static/index/icon_song_right.png';
 //背景音频
-const backgroundAudioManager:any = Taro.getBackgroundAudioManager();
+const backgroundAudioManager: any = Taro.getBackgroundAudioManager();
 const Detail: React.FC = () => {
     useLoad((e: any) => {
         getMusicUrlFunc(e.id);
         getMusicDetailFunc(e.id);
     })
+    useUnload(() => {
+        backgroundAudioManager.stop();
+    })
     let [songImg, setSongImg]: [songImg: string, setSongImg: any] = useState('');
     let [ifPlay, setIfPlay]: [ifPlay: boolean, setIfPlay: any] = useState(false);
+    let [ifToast, setIfToast]: [ifToast: boolean, setIfToast: any] = useState(false);
     const getMusicUrlFunc = async (id: string) => {
         const [err, res]: any = await getMusicUrlApi({
             id,
@@ -54,9 +58,14 @@ const Detail: React.FC = () => {
             backgroundAudioManager.play();
         }
     };
+    //显示警告信息
+    const showToast = () => {
+        setIfToast(true);
+    };
     return (
         <View className="songIndex">
             <NavBar className="songIndex-nav" leftShow={false}></NavBar>
+            <Toast visible={ifToast} msg={'暂不支持'} type={'warn'} onClose={()=>{setIfToast(false)}}></Toast>
             <View className="songIndex-top">
                 <Image src={leverImg} className="songIndex-top-img"></Image>
             </View>
@@ -69,15 +78,15 @@ const Detail: React.FC = () => {
             </View>
             <View className="songIndex-bottom">
                 <View className="songIndex-bottom-left">
-                    <Image src={randomImg} className="songIndex-bottom-left-img"></Image>
+                    <Image src={randomImg} className="songIndex-bottom-left-img" onClick={showToast}></Image>
                 </View>
                 <View className="songIndex-bottom-middle">
-                    <Image src={leftImg} className="songIndex-bottom-middle-img"></Image>
+                    <Image src={leftImg} className="songIndex-bottom-middle-img" onClick={showToast}></Image>
                     <Image src={ifPlay ? (playImg) : (pauseImg)} className="songIndex-bottom-middle-img" onClick={pauseAndPlay}></Image>
-                    <Image src={rightImg} className="songIndex-bottom-middle-img"></Image>
+                    <Image src={rightImg} className="songIndex-bottom-middle-img" onClick={showToast}></Image>
                 </View>
                 <View className="songIndex-bottom-right">
-                    <Icon name="heart-fill" color="rgba(255, 30, 0, 1)" size={40} className="nut-icon-am-breathe  nut-icon-am-infinite" />
+                    <Icon onClick={showToast} name="heart-fill" color="rgba(255, 30, 0, 1)" size={40} className="nut-icon-am-breathe  nut-icon-am-infinite" />
                 </View>
             </View>
         </View>
